9b66cef0b4a1bdc92a1c82f15f6e4476776e4720
{Gabriel Norman} <{gabriel@superherocheesecake.com}>
2021-06-01 20:24:23 +0200

    version 1.0.1

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -4,16 +4,19 @@ let tl = gsap.timeline({paused: true, reversed: true});
 const name = 'setCookie';
 const cookie = localStorage.getItem(name);
 
-tl.to(bisBar, {autoAlpha: 1, duration: .65, ease: "expo.inOut"}, "same")
-    .to(bisBar, {rotate: 0, duration: .4, ease: "expo.inOut"}, "same")
-
-if(cookie == null) {
-    tl.play();
+if(bisBar) {
+    tl.to(bisBar, {autoAlpha: 1, duration: .65, ease: "expo.inOut"}, "same")
+        .to(bisBar, {rotate: 0, duration: .4, ease: "expo.inOut"}, "same")
+    
+    if(cookie == null) {
+        tl.play();
+    }
+    
+    btn.addEventListener('click', ()=> {
+        tl.reverse();
+        if(cookie == null) {
+            localStorage.setItem(name, 1);
+        };
+    });
 }
 
-btn.addEventListener('click', ()=> {
-    tl.reverse();
-    if(cookie == null) {
-        localStorage.setItem(name, 1);
-    };
-});
